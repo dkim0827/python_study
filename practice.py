368aa29789511946f7fed7ba7d75bc3ecd94e647
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-23 00:07:13 -0800

    added different data type & way to comment

--- practice.py
+++ practice.py
@@ -5,3 +5,35 @@ print(1000)
 print(5 + 3)
 print(2 * 8)
 print(3 * (3 + 1))
+
+print('balloon')
+print('hello' * 9)
+
+print(5 > 10)
+print(5 < 10)
+print(True)
+print(False)
+
+print(not True)
+print(not False)
+print(not (5 > 10))
+
+#introduce your pet
+animal = "dog"
+name = "salang"
+age = 4
+hobby = "walk"
+is_adult = age >= 3
+
+print("my " + animal + " is " + name)
+print(name, "is", age, "years old and", name, "love to", hobby)
+print("is " + name + " old enough? " + str(is_adult))
+
+# for boolean and number need to str(age)
+# if use , instead of + dont need to put str but , automatically add space
+
+'''
+how to multi line comment
+'''
+
+# how to do single line comment
