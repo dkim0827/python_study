5045c2721ace25b03ae9141c76339e03675ad5b9
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-23 00:18:15 -0800

    arithmatic operator

---
+++ calculate.py
@@ -0,0 +1,39 @@
+print(1+1) # 2
+print(3-2) # 1
+print(5*2) # 10
+print(6/3) # 2
+
+print(2**3) # 8 - power
+print(5 % 3) # 2 - how to get remainder
+print(10 % 3) # 1
+
+print(5 // 3) # 1 - how to calculate how many times it goes in
+print(10 // 3) # 3
+
+print(10 > 3) # True
+print(4 >= 7) # False
+print(5 <= 5) # True
+
+print(3 == 3) # True
+print(4 == 2) # False
+print(3 + 4 == 7) # True
+
+print(1 != 3) # True
+print(not(1 != 3)) # False
+
+print((3 > 0) and (3 < 5)) # True
+print((3 > 0) & (3 < 5)) # True - different with javascript
+
+print((3 > 0) or (3 > 5)) # True
+print((3 > 0) | (3 > 5)) # True - different with javascript
+
+print(5 > 4 > 3) # True
+print(5 > 4 > 7) # False
+
+number = 2 + 3 * 4 # 14
+print(number)
+number = number + 2 # 16
+print(number)
+# number = number + 2 == number += 2
+number += 2
+print(number)
