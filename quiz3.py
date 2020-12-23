cea374e850c8aff79f1b98f984415327d1b63803
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-23 01:29:54 -0800

    added solution for quiz3

--- quiz3.py
+++ quiz3.py
@@ -12,5 +12,15 @@ password : nav51!
 
 site = "https://naver.com"
 word = site[8:site.find(".")]
-password = word[0:3] + str(len(word)) + str(word.count("e")) + "!"
-print(f"password : {password}")
+password = word[:3] + str(len(word)) + str(word.count("e")) + "!"
+print(f"site : {site} | password : {password}")
+
+'''
+solution
+url = "https://naver.com"
+my_str = url.replace("https://", "")
+my_str = my_str[:mystr.index(".")]
+password = my_str[:3] + str(len(my_str)) + str(my_str.count("e")) + "!"
+print("{0}'s password is {1}".format(url, password))
+
+'''
