afbbfe51fa6ba435065072093639f90afb0573b0
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-23 18:39:22 -0800

    tupples

---
+++ tupples.py
@@ -0,0 +1,14 @@
+menu = ("ton_katsu, chicken_katsu")
+print(menu[0])
+print(menu[1])
+
+# menu.add("fish_katsu")
+
+# name = "kim"
+# age = 20
+# hobby = "coding"
+# print(name, age, hobby)
+
+(name, age, hobby) = ("kim", 20, "coding")
+print(name, age, hobby)
+
