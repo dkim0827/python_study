3f202c3f8127b3060398d03f960e8833050508fd
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-23 18:48:56 -0800

    change_data in python

---
+++ change_data.py
@@ -0,0 +1,18 @@
+# how to change information
+
+# coffee shop
+# set
+menu = {"coffee", "milk", "juice"}
+print(menu, type(menu))
+
+# list
+menu = list(menu)
+print(menu, type(menu))
+
+# tuple
+menu = tuple(menu)
+print(menu, type(menu))
+
+# set
+menu = set(menu)
+print(menu, type(menu))
