8f929d52827bf8b877c6b0709ed2c87d2644f021
Daniel Kim <danielkim@Daniels-MacBook-Pro.local>
2020-12-24 23:38:25 -0800

    how to create package

--- travel/thailand.py
+++ travel/thailand.py
@@ -1,3 +1,12 @@
 class ThailandPackage:
     def detail(self):
         print("Thailand Travel Package")
+
+if __name__ == "__main__": # if this name is main
+    print("Thailand module started")
+    print("this sentence only kicks when module runs itself")
+    trip_to = ThailandPackage()
+    trip_to.detail()
+
+else:
+    print("Thailand moudle has been called from outside of the module")
